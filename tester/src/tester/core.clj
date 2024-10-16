5cc9742645df3563a85d516a8f4066579de85f43
Joel Martin <github@martintribe.org>
2024-10-16 00:06:30 -0400

    tester: Use instacheck.cli 0.9.3. Add parse cmd.
    Major refactoring to use instacheck.cli from instacheck 0.9.3 which
    allows use of the cli routines externally in a more friendly way (no
    usage, ability to pass a check-fn to do-check, etc).
    
    Use the instacheck.cli "library" for the check command and the parse
    command. Support a bunch of standard command line options (see the usage
    docstring).
    
    Add a `bin/tester` wrapper that invokes the clojure standard image to
    run the deps.edn based tester command. Has the deps config setup so that
    it runs from the top instead of inside the tester directory. Results
    from checks will be in the `samples/` directory by default.
    
    Improve URL handling so that a trailing slash in the destinations won't
    break things.

--- tester/src/tester/core.clj
+++ tester/src/tester/core.clj
@@ -2,15 +2,53 @@
   (:require [clojure.string :as S]
             [clojure.data.json :as json]
             [clojure.pprint :refer [pprint]]
+            [clojure.edn :as edn]
+
+            [docopt.core :as docopt]
             [org.httpkit.client :as hk]
-            [instacheck.core :as instacheck]))
+            [instaparse.core :as instaparse]
+            [instacheck.grammar :as igrammar]
+            [instacheck.cli :as instacheck]))
+
+(def usage "HTTP Endpoint Tester
+
+Usage:
+  tester check [OPTIONS] <dest1> <dest2>
+  tester parse [OPTIONS] <inputs>...
+
+Options:
+  -h --help           Show this help
+  -v --verbose        Verbose output
+  --ebnf-file=FILE    EBNF file that defines HTTP requests
+                      [default: ./actions.ebnf]
+  --ebnf-output=FILE  EBNF file that defines HTTP requests
+                      [default: ./actions.ebnf]
+  --runs=NUM          Number of test runs
+                      [default: 1]
+  --iterations=NUM    Number of test iterations per run
+                      [default: 10]
+  --sample-dir=DIR    Output samples/inputs to this dir
+                      [default: samples]
+  --weights-in=FILE   Weights file (EDN) to override EBNF weights
+  --weights-out=FILE  Output weights file (EDN) with fail case weights.
+")
+
+(defn clean-opts [arg-map]
+  (reduce (fn [o [a v]]
+            (let [k (keyword (S/replace a #"^[-<]*([^>]*)[>]*$" "$1"))]
+              (assoc o k (or (get o k) v))))
+          {} arg-map))
 
 (defn body->json
   [body]
   (when (and body (not= "" body)) (json/read-str body)))
 
-(defn run-actions [actions-str dest1 dest2]
-  (let [actions (json/read-str actions-str :key-fn keyword)
+(defn url [base path]
+  (.toString (.resolve (java.net.URI. base) path)))
+
+(defn run-actions [ctx dest1 dest2 sample-path]
+  (let [actions-str (slurp sample-path)
+        actions (json/read-str actions-str :key-fn keyword)
         all-actions (concat [{:method "PUT" :path "/reset"}]
                             actions
                             [{:method "GET" :path "/users"}])]
@@ -21,35 +59,46 @@
                   :headers {"Content-Type" "application/json"}
                   :body (when payload (json/write-str payload))}
 
-            req1 (hk/request (assoc base :url (str dest1 path)))
-            req2 (hk/request (assoc base :url (str dest2 path)))
+            req1 (hk/request (assoc base :url (url dest1 path)))
+            req2 (hk/request (assoc base :url (url dest2 path)))
             [resp1 resp2] [@req1 @req2]
             [body1 body2] (map (comp body->json :body) [resp1 resp2])
-            result (and (= (:status resp1) (:status resp2))
+            pass? (and (= (:status resp1) (:status resp2))
                         (= body1 body2))]
-        ;;(prn :RUN :method method :path path :statuses (map :status [resp1 resp2]) :-> result)
-        (if (and result (seq actions))
+        (when (:verbose ctx)
+          (prn :run-actions :method method :path path
+               :statuses (map :status [resp1 resp2]) :-> pass?))
+        (if (and pass? (seq actions))
           (recur actions)
-          result)))))
+          pass?)))))
 
-(defn report-fn [result]
-  #(println :report (select-keys % [:failed-after-ms
-                                    :num-tests
-                                    :type
-                                    :seed
-                                    :fail
-                                    :failing-size
-                                    :pass?
-                                    ;;:property
-                                    :result
-                                    :result-data
-                                    :shrinking])))
+(defn run [opts]
+  (let [{:keys [check parse verbose runs iterations
+                weights-in ebnf-file sample-dir
+                dest1 dest2
+                ebnf-output inputs]} opts
+        _ (when verbose (println "Opts:" (pprint opts)))
+        file-weights (when weights-in (edn/read-string (slurp weights-in)))
+        actions-parser (instaparse/parser (slurp ebnf-file))
+        comment-weights (igrammar/comment-trek
+                          (igrammar/parser->grammar actions-parser)
+                          :weight)
+        ctx (merge {:verbose verbose
+                    :weights (merge comment-weights file-weights)
+                    :weights-res (atom {})
+                    :ebnf-output ebnf-output}
+                   (when (:verbose opts)
+                     {:log-fn instacheck/pr-err}))
+        check-fn (fn [ctx sample-path]
+                     (run-actions ctx dest1 dest2 sample-path))]
+    (cond
+      check
+      (instacheck/do-check ctx actions-parser sample-dir check-fn
+                           (merge opts {:runs (Integer. runs)
+                                        :iterations (Integer. iterations)}))
 
-(defn -main [ebnf-file dest1 dest2 & args]
-  (let [ebnf (slurp ebnf-file)
-        actions-gen (instacheck/ebnf->gen ebnf)
-        result (instacheck/instacheck
-                 #(run-actions % dest1 dest2) actions-gen {:iterations 20
-                                                           :report-fn report-fn})]
-    (println "Result:")
-    (pprint result)))
+      parse
+      (instacheck/do-parse ctx actions-parser inputs))))
+
+(defn -main [& args]
+  (docopt/docopt usage args #(run (clean-opts %))))
