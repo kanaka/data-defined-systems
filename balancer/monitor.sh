6ea19ce77911bf82697f5af33b2ae009c3a5e890
Joel Martin <github@martintribe.org>
2024-10-20 23:01:15 -0400

    balancer/monitor.sh: restart listener on error.

--- balancer/monitor.sh
+++ balancer/monitor.sh
@@ -14,19 +14,23 @@ while ! nats stream info events; do
   sleep 1
 done
 
-nats consumer sub -r events balancer | \
-while IFS= read message; do
-  echo "NATS message: ${message}"
-  action=$(echo "${message}" | jq -r '.action')
-  target=$(echo "${message}" | jq -r '.target')
-  name=$(echo "${target}" | tr '.: ' '_')
-  file=/tmp/servers/${name}.cfg
-  case "${action}" in
-    add)    echo "  server svr_${name} ${target} check" > ${file} ;;
-    delete) rm -f ${file} ;;
-  esac
+while true; do
+  nats consumer sub -r events balancer | \
+  while IFS= read message; do
+    echo "NATS message: ${message}"
+    action=$(echo "${message}" | jq -r '.action')
+    target=$(echo "${message}" | jq -r '.target')
+    name=$(echo "${target}" | tr '.: ' '_')
+    file=/tmp/servers/${name}.cfg
+    case "${action}" in
+      add)    echo "  server svr_${name} ${target} check" > ${file} ;;
+      delete) rm -f ${file} ;;
+    esac
 
-  # Update the config and restart haproxy
-  cat /etc/haproxy-base.cfg /tmp/servers/*.cfg > /tmp/haproxy.cfg
-  kill -USR2 ${svr_pid}
+    # Update the config and restart haproxy
+    cat /etc/haproxy-base.cfg /tmp/servers/*.cfg > /tmp/haproxy.cfg
+    kill -USR2 ${svr_pid}
+  done
+  echo "ERROR: consumer died, restarting"
+  sleep 1
 done
